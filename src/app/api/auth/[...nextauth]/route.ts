88744a806172d16200336757abccdc651f58ef22
Syed Hasnain Mehadi <hasnain.mehadi.con@ampersandgroup.in>
2025-09-16 14:40:48 +0530

    added key cloack loginc

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,10 @@
-// // app/api/auth/[...nextauth].js
-//app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
 import KeycloakProvider from "next-auth/providers/keycloak";
 
-if (!process.env.KEYCLOAK_CLIENT_ID) {
-  throw new Error("KEYCLOAK_CLIENT_ID is not set");
-}
-if (!process.env.KEYCLOAK_CLIENT_SECRET) {
-  throw new Error("KEYCLOAK_CLIENT_SECRET is not set");
-}
-if (!process.env.KEYCLOAK_ISSUER) {
-  throw new Error("KEYCLOAK_ISSUER is not set");
-}
-if (!process.env.NEXTAUTH_URL) {
-  throw new Error("NEXTAUTH_URL is not set");
-}
-if (!process.env.NEXTAUTH_SECRET) {
-  throw new Error("NEXTAUTH_SECRET is not set");
+declare module "next-auth" {
+  interface Session {
+    accessToken: unknown;
+  }
 }
 
 const handler = NextAuth({
@@ -27,6 +15,19 @@ const handler = NextAuth({
       issuer: process.env.KEYCLOAK_ISSUER!,
     }),
   ],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      console.log("profile :>> ", profile);
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
